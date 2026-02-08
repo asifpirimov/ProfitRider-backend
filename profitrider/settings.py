878588aa331c7d0c9714ce97936658e5ac22ed8f
unknown <asifpirimov99@gmail.com>
2026-02-08 23:00:11 +0400

    Fix: Add AUTHENTICATION_BACKENDS to settings.py for Google Login

--- profitrider/settings.py
+++ profitrider/settings.py
@@ -56,6 +56,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'profitrider.urls'
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
